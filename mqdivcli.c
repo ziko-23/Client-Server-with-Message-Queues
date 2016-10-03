1bbf89c708aefe756887a46cea5246c1641fafe7
LeiTi34 <leiti34@gmail.com>
2016-10-03 07:57:47 -0700

    Übertragung abschließen

--- mqdivcli.c
+++ mqdivcli.c
@@ -1,3 +1,5 @@
+/* Autor: Alex Leidwein */
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -10,40 +12,108 @@
 extern int errno;
 #define MSGPERM 0600
 
-int msgqid, rc;
+int msgqid_rcv, msgqid_snd, rc;
 int done;
 time_t _ltime;
 
-struct q_type {
+struct q_snd {
   long mtype;
-  char mtext[81];
-}q_entry;
+  int  client_kennung, genauigkeit, dividend, divisor;
+}snd;
+
+struct q_rcv {
+  long mtype;
+  double test;
+}rcv;
 
 int main(int argc,char **argv)
 {
-  //msgqid=msgget(IPC_PRIVATE,MSGPERM|IPC_CREAT|IPC_EXCL);
-  /* create message queue, abort if message queue with the same key exists */
-  /*if (msgqid < 0) {
-    perror( strerror(errno) );
-    printf("msgget failed, msgqid = %d\n", msgqid);
-    exit(1);
-  }*/
-  msgqid = msgget(23, IPC_PRIVATE);
-  printf("message queue %d created\n",msgqid);
+    int i;
 
-   ///Recive
-  rc=msgrcv(msgqid,&q_entry,80,0,0);
-  if (rc < 0) {
-    perror( strerror(errno) );
-    printf("msgrcv failed, rc=%d\n", rc);
-    exit(1);
-  } 
-  printf("received msg: %s\n", q_entry.mtext);
-   rc=msgctl(msgqid,IPC_RMID,NULL);
-  if (rc < 0) {
-    perror( strerror(errno) );
-    printf("msgctl (return queue) failed, rc=%d\n", rc);
-    exit(1);
-  }
-  printf("message queue %d is gone\n",msgqid);
+    if (argc > 1)
+    {
+      for (i = 1; i < argc; i++)
+	    { 
+	      switch(argv[i][1])
+        {
+          case 'g':  //Genauigkeit
+            i++;
+            snd.genauigkeit = atoi(argv[i]);
+            break;
+            
+          case 'd':  //Divident
+            i++;
+            snd.dividend = atoi(argv[i]);
+            break;
+
+          case 's':  //Divisor
+            i++;
+            snd.divisor = atoi(argv[i]);
+            break;
+
+          case 't':  //Divisor
+            i++;
+            snd.mtype = atoi(argv[i]);
+            break;  
+        }
+	    }
+    }
+
+    ///Open
+    msgqid_snd = msgget(1, IPC_PRIVATE | MSGPERM);
+    //msgqid=msgget(IPC_PRIVATE,MSGPERM|IPC_CREAT|IPC_EXCL);
+    /* create message queue, abort if message queue with the same key exists */
+    if (msgqid_snd < 0) {
+      perror( strerror(errno) );
+      printf("msgget failed, msgqid_snd = %d\n", msgqid_snd);
+      exit(1);
+    }
+    printf("message queue %d opened\n",msgqid_snd);
+
+    if(snd.mtype != 1000)///Create
+    {
+      msgqid_rcv = msgget(snd.mtype, IPC_PRIVATE | IPC_CREAT | MSGPERM);
+        if (msgqid_rcv < 0) {
+          perror( strerror(errno) );
+          printf("msgget failed, msgqid_rcv = %d\n", msgqid_rcv);
+          exit(1);
+        }
+        printf("message queue %d created\n",msgqid_rcv);
+    }
+
+    ///Send
+    rc = msgsnd(msgqid_snd, &snd, sizeof(snd) - sizeof(long), 0);
+    /* take IPC_NOWAIT instead of 0, if you want the process not to suspend, 
+    if message delivery is not possible at the moment */
+
+    if (rc < 0) {
+        perror( strerror(errno) );
+        printf("msgsnd failed, rc = %d\n", rc);
+        exit(1);
+    }
+    printf("sent msg: %d/%d, %d, %ld\n", snd.dividend,snd.divisor,snd.genauigkeit,snd.mtype); 
+
+/********************************************************************************/
+    if(snd.mtype != 1000)
+    {
+      ///Recive
+      rc=msgrcv(msgqid_rcv,&rcv,sizeof(rcv) - sizeof(long),0,0);
+      if (rc < 0) {
+        perror( strerror(errno) );
+        printf("msgrcv failed, rc=%d\n", rc);
+        exit(1);
+      } 
+      printf("received msg:\t%f\n", rcv.test);
+
+      ///Beenden
+      rc=msgctl(msgqid_rcv,IPC_RMID,NULL);
+      if (rc < 0) {
+          perror( strerror(errno) );
+          printf("msgctl (return queue) failed, rc=%d\n", rc);
+          exit(1);
+        }
+        printf("message queue %d is gone\n",msgqid_rcv);
+    }
+
+    exit(0);
 }
--- mqdivser.c
+++ mqdivser.c
@@ -1,3 +1,5 @@
+/* Autor: Alex Leidwein */
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -6,42 +8,87 @@
 #include<sys/msg.h>
 #include<sys/wait.h>
 #include<sys/errno.h>
+#include<math.h>
      
 extern int errno;
 #define MSGPERM 0600
 
-int msgqid, rc;
+int msgqid_rcv, msgqid_snd, rc;
 int done;
 time_t _ltime;
 
 struct q_type {
-  long mtype;
-  char mtext[81];
-}q_entry;
+    long mtype;
+    int  client_kennung, genauigkeit, dividend, divisor;
+}rcv;
+
+struct q_rcv {
+    long mtype;
+    double  test;
+}snd;
 
 int main(int argc,char **argv)
 {
-    msgqid = msgget(23, IPC_CREAT | IPC_PRIVATE);
     //msgqid=msgget(IPC_PRIVATE,MSGPERM|IPC_CREAT|IPC_EXCL);
     /* create message queue, abort if message queue with the same key exists */
-    if (msgqid < 0) {
-      perror( strerror(errno) );
-      printf("msgget failed, msgqid = %d\n", msgqid);
-      exit(1);
-    }
-    printf("message queue %d created\n",msgqid);
-    q_entry.mtype=1; /* set the type of message */
-    _ltime = time(&_ltime); /* retrieving current time by calling time() */
-    sprintf (q_entry.mtext,"%s\n",ctime(&_ltime)); /* setting the right time format by means of ctime() */
-
-    ///Send
-    rc = msgsnd(msgqid,&q_entry,80,0);
-    /* take IPC_NOWAIT instead of 0, if you want the process not to suspend, 
-    if message delivery is not possible at the moment */
-
-    if (rc < 0) {
+    /*if (msgqid < 0) {
         perror( strerror(errno) );
-        printf("msgsnd failed, rc = %d\n", rc);
+        printf("msgget failed, msgqid = %d\n", msgqid);
+        exit(1);
+    }*/
+    ///Create
+    msgqid_rcv = msgget(1, IPC_PRIVATE | IPC_CREAT | MSGPERM);
+    if (msgqid_rcv < 0) {
+        perror( strerror(errno) );
+        printf("msgget failed, msgqid_rcv = %d\n", msgqid_rcv);
         exit(1);
     }
+    printf("message queue %d created\n",msgqid_rcv);
+
+    for(;;) ///Recive
+    {
+        rc=msgrcv(msgqid_rcv,&rcv,sizeof(rcv) - sizeof(long),0,0);
+        if (rc < 0) {
+            perror( strerror(errno) );
+            printf("msgrcv failed, rc=%d\n", rc);
+            exit(1);
+        } 
+
+        if(rcv.mtype == 1000) ///Beenden
+        {
+            rc=msgctl(msgqid_rcv,IPC_RMID,NULL);
+            if (rc < 0) {
+                perror( strerror(errno) );
+                printf("msgctl (return queue) failed, rc=%d\n", rc);
+                exit(1);
+            }
+            printf("message queue %d is gone\n",msgqid_rcv);
+            exit(0);
+        }
+        printf("received msg: %d/%d, %d, %ld\n", rcv.dividend,rcv.divisor,rcv.genauigkeit, rcv.mtype); 
+    /***********************************/ //TODO
+        ///Open
+        msgqid_snd = msgget(rcv.mtype, IPC_PRIVATE | MSGPERM);
+
+        if (msgqid_snd < 0) {
+            perror( strerror(errno) );
+            printf("msgget failed, msgqid = %d\n", msgqid_snd);
+            exit(1);
+        }
+        printf("message queue %d opened\n",msgqid_snd);
+
+        snd.test = (double)rcv.dividend/(double)rcv.divisor;
+        snd.mtype = 1;
+
+        ///Send
+        rc = msgsnd(msgqid_snd, &snd, sizeof(snd) - sizeof(long), 0);
+        if (rc < 0) {
+            perror( strerror(errno) );
+            printf("msgsnd failed, rc = %d\n", rc);
+            exit(1);
+        }
+        printf("sent msg:\t%f\n", snd.test); 
+        //printf("sent msg: %s\n", q_entry.mtext);       
+    }
 }
+
