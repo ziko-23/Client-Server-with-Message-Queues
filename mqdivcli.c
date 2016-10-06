17d57fab0257ff32e6353ac7666cb897c2b0e99b
LeiTi34 <leiti34@gmail.com>
2016-10-06 03:05:08 -0700

    Added better Comments

--- mqdivcli.c
+++ mqdivcli.c
@@ -1,4 +1,9 @@
-/* Autor: Alex Leidwein */
+/************************
+ * Message Queue Devide *
+ * Client               *
+ *                      *
+ * Autor: Alex Leidwein *
+ ************************/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,7 +17,8 @@
 #include <sys/errno.h>
      
 extern int errno;
-#define MSGPERM 0666
+
+#define MSGPERM 0600  //Read(0400) | Write(0200) Permission
 
 int main(int argc,char **argv)
 {
@@ -20,11 +26,13 @@ int main(int argc,char **argv)
     int msgqid_rcv, msgqid_snd, rc, i;
     pid_t pid;
 
-    struct q_snd {
+    //Send Message
+    struct q_snd { 
       long mtype;
       int  client_kennung, genauigkeit, dividend, divisor;
     }snd;
 
+    //Recive Message
     struct q_rcv {
       long mtype;
       double test;
@@ -37,13 +45,13 @@ int main(int argc,char **argv)
     snd.mtype = 0;
 
     //Argumente
-    if (argc > 1)
+    if (argc > 1) //Überprüfen ob Argumente übergeben wurden
     {
       for (i = 1; i < argc; i++)
 	    { 
         if (argv[i][0] == '-' & argv[i][2] == '\0')
         {
-          switch(argv[i][1])
+          switch(argv[i][1])  //Argumente in Variabln Speichern
           {
             case 'g':  //Genauigkeit
               i++;
@@ -69,7 +77,8 @@ int main(int argc,char **argv)
                 exit(1);
               }
               break;  
-    } }
+          } 
+        }
       }
     }
     if(snd.mtype == 0)
@@ -79,12 +88,11 @@ int main(int argc,char **argv)
     }
 
     //Open Send
-    msgqid_snd = msgget(1, MSGPERM);
-    if (msgqid_snd < 0) 
+    msgqid_snd = msgget(1, MSGPERM);  //Message Queue mit Key=1 öffnen
+    if (msgqid_snd < 0) //Starte server wenn mque nicht erreichbar
     {
       printf("[C] msgget failed, msgqid_snd = %d\n", msgqid_snd);
 
-      //Starte server wenn mque nicht erreichbar (NOT WORKING)
       pid = fork();
 
       switch(pid)
@@ -100,27 +108,27 @@ int main(int argc,char **argv)
 
         printf("[C] retry to open message queue\n");
         msgqid_snd = msgget(1, MSGPERM);
-        if (msgqid_snd < 0) {
+        if (msgqid_snd < 0) { //Öffnen erneut Fehlgeschlagen
           perror( strerror(errno) );
-          printf("[C] msgget failed, msgqid_snd = %d\n", msgqid_snd);
+          printf("[C] msgget failed, msgqid_snd = %d\n", msgqid_snd); //Message Queue erneut öffnen
           exit(1);
         }
         break;
 
       default: //Child
           printf("[C] starting server with PID = %d", getpid());
-          execl("./mqdivser.out", "./mqdivser.out", NULL);
+          execl("./mqdivser.out", "./mqdivser.out", NULL);  //Server Starten
           exit(1);
 
       }
     }
-    printf("[C] message queue %d opened\n",msgqid_snd);
+    printf("[C] message queue %d opened\n",msgqid_snd); //Öffnen Erfolgreich
 
     //Create Recive
-    if(snd.mtype != 1000)
+    if(snd.mtype != 1000) //Erstelle keine Message Queue wenn server geschlossen wird
     {
-      msgqid_rcv = msgget(snd.mtype, IPC_CREAT | MSGPERM);
-      if (msgqid_rcv < 0) {
+      msgqid_rcv = msgget(snd.mtype, IPC_CREAT | MSGPERM);  //Erstelle Message Queue mit Key=Type 
+      if (msgqid_rcv < 0) { //Erstellen fehlgeschlagen
         perror( strerror(errno) );
         printf("[C] msgget failed, msgqid_rcv = %d\n", msgqid_rcv);
         exit(1);
@@ -129,8 +137,8 @@ int main(int argc,char **argv)
     }
 
     //Send
-    rc = msgsnd(msgqid_snd, &snd, sizeof(snd) - sizeof(long), 0);
-    if (rc < 0) {
+    rc = msgsnd(msgqid_snd, &snd, sizeof(snd) - sizeof(long), 0); //Sende Message
+    if (rc < 0) { //Senden Fehlgeschlagen
       perror( strerror(errno) );
       printf("[C] msgsnd failed, rc = %d\n", rc);
       exit(1);
@@ -138,11 +146,11 @@ int main(int argc,char **argv)
     printf("[C] sent msg:     %ld: %d/%d, %d\n", snd.mtype, snd.dividend, snd.divisor, snd.genauigkeit); 
 
 
-    if(snd.mtype != 1000)
+    if(snd.mtype != 1000) //Warte nicht auf Message wenn server geschlossen wird
     {
       //Recive
-      rc=msgrcv(msgqid_rcv,&rcv,sizeof(rcv) - sizeof(long),0,0);
-      if (rc < 0) {
+      rc=msgrcv(msgqid_rcv,&rcv,sizeof(rcv) - sizeof(long),0,0);  //Warte Auf Message von Server
+      if (rc < 0) { //Empfangen fehlgeschlagen
         perror( strerror(errno) );
         printf("[C] msgrcv failed, rc=%d\n", rc);
         exit(1);
@@ -150,8 +158,8 @@ int main(int argc,char **argv)
       printf("[C] received msg: %f\n", rcv.test);
 
       //Beenden Recive
-      rc=msgctl(msgqid_rcv,IPC_RMID,NULL);
-      if (rc < 0) {
+      rc=msgctl(msgqid_rcv,IPC_RMID,NULL);  //Message Queue Beenden
+      if (rc < 0) { //Beenden Fehlgeschlagen
         perror( strerror(errno) );
         printf("[C] msgctl (return queue) failed, rc=%d\n", rc);
         exit(1);
--- mqdivser.c
+++ mqdivser.c
@@ -1,4 +1,10 @@
- /* Autor: Alex Leidwein */
+/************************
+ * Message Queue Devide *
+ * Server               *
+ *                      *
+ * Autor: Alex Leidwein *
+ *        Felix Eilmer  *
+ ************************/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,37 +17,40 @@
 #include <math.h>
      
 extern int errno;
-#define MSGPERM 0600
 
-int main(int argc,char **argv)
+#define MSGPERM 0600    //Read(0400) | Write(0200) Permission
+
+int main()
 { 
     //Variablendefinitionen  
     int msgqid_rcv, msgqid_snd, rc, i, pot = 1;
 
+    //Recive Message
     struct q_rcv {
         long mtype;
         int  client_kennung, genauigkeit, dividend, divisor;
     }rcv;
 
+    //Send Message
     struct q_snd {
         long mtype;
         double  test;
     }snd;
 
     //Create Recive
-    msgqid_rcv = msgget(1, IPC_CREAT | MSGPERM);
-    if (msgqid_rcv < 0) {
+    msgqid_rcv = msgget(1, IPC_CREAT | MSGPERM);    //Message Queue mit Key=1 erstellen
+    if (msgqid_rcv < 0) {   //Erstellen Fehlgeschlagen
         perror( strerror(errno) );
         printf("[S] msgget failed, msgqid_rcv = %d\n", msgqid_rcv);
         exit(1);
     }
     printf("[S] message queue %d created\n",msgqid_rcv);
 
-    for(;;)
+    for(;;) //Endlosschleife
     {
         //Recive
-        rc=msgrcv(msgqid_rcv,&rcv,sizeof(rcv) - sizeof(long),0,0);
-        if (rc < 0) {
+        rc=msgrcv(msgqid_rcv,&rcv,sizeof(rcv) - sizeof(long),0,0);  //Empfange Message
+        if (rc < 0) {   //Empfangen fehlgeschlagen
             perror( strerror(errno) );
             printf("[S] msgrcv failed, rc=%d\n", rc);
             exit(1);
@@ -49,48 +58,50 @@ int main(int argc,char **argv)
         printf("[S] received msg: %ld: %d/%d, %d\n", rcv.mtype, rcv.dividend,rcv.divisor,rcv.genauigkeit); 
 
         //Beenden Recive
-        if(rcv.mtype == 1000)
+        if(rcv.mtype == 1000)   //Wenn Type der empfangenen Nachricht = 1000 -> Beende Server
         {
-            rc=msgctl(msgqid_rcv,IPC_RMID,NULL);
-            if (rc < 0) {
+            rc=msgctl(msgqid_rcv,IPC_RMID,NULL);    //Message Queue mit Key=1 Schließen
+            if (rc < 0) {   //Schließen Fehlgeschlagen
                 perror( strerror(errno) );
                 printf("[S] msgctl (return queue) failed, rc=%d\n", rc);
                 exit(1);
             }
             printf("[S] message queue %d is gone\n",msgqid_rcv);
 
-            break;
+            break;  //Springe aus Endlosschleife
         }
         
         //Open Send
-        msgqid_snd = msgget(rcv.mtype, MSGPERM);
-
-        if (msgqid_snd < 0) {
+        msgqid_snd = msgget(rcv.mtype, MSGPERM);    //Öffne Message Queue mit Key=Type
+        if (msgqid_snd < 0) {   //Öffnen Fehlgeschlagen
             perror( strerror(errno) );
             printf("[S] msgget failed, msgqid = %d\n", msgqid_snd);
             exit(1);
         }
         printf("[S] message queue %d opened\n",msgqid_snd);
 
+
+        snd.mtype = 1;  //Type Festlegen
+
         //Berechenen	
-        snd.mtype = 1;
-        snd.test = (double)rcv.dividend/(double)rcv.divisor;
+        snd.test = (double)rcv.dividend/(double)rcv.divisor;    //Division
+
 	    pot = 1;	
+
 	    for(i=0;i<rcv.genauigkeit;i++) {
 		    pot *= 10;
 	    }
-	    snd.test = (int)(diverg*pot);
-	    snd.test /= pot;
+	    snd.test = (int)(diverg*pot);   //"Schiebe" Werte mit 10^(genauigkeit) nach links. Typecast int um Kommastellen abzuschneiden
+	    snd.test /= pot;    //Dividiere durch 10^(genauigkeit) um Originalwert mit Eingestellter Genauigkeit herzustellen
 
         //Send
-        rc = msgsnd(msgqid_snd, &snd, sizeof(snd) - sizeof(long), 0);
-        if (rc < 0) {
+        rc = msgsnd(msgqid_snd, &snd, sizeof(snd) - sizeof(long), 0);   //Sende berechneten Wert
+        if (rc < 0) {   //Senden Fehlgeschlagen
             perror( strerror(errno) );
             printf("[S] msgsnd failed, rc = %d\n", rc);
             exit(1);
         }
         printf("[S] sent msg:     %f\n", snd.test); 
-        //printf("sent msg: %s\n", q_entry.mtext);       
     }
 
     printf("[S] Exit\n");
